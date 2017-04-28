0999538e8016188743410d76b985f67e09f4e93f
System Administrator <root@iMac-de-Hedrei.local>
2017-04-27 21:39:14 -0300

    Comentarios

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -7,10 +7,10 @@ use Slim\Views\Twig as View;
 class HomeController extends Controller {
 
 	public function index($request, $response) {
-		$home = new \App\Models\Home;
-		$data = $home->index();
+		$modelHome = new \App\Models\Home;
+		$data = $modelHome->index();
 
-		print_r( $home->index());
+		print_r( $data );
 		//http://api-doeobem:8888/public/?name=hedrei
 		//var_dump($request->getPaam('name'));
 
--- app/routes.php
+++ app/routes.php
@@ -1,19 +1,11 @@
 <?php
 
 
-// chamando controller que passa por model e printa retorno do model
-// sem interacao com o view
 $app->get('/', 'homeController:index');
 
-// Chamando controler que passa por model e retorna informacao
-/// Verificar como enviar essa informacao array() para o home.html(view)
 $app->get('/home', function ($request, $response) {
 
-	$homeController = new homeController();
-	$dataControlModelHome = $homeController->index();
-
-
-    return $this->view->render($response, 'home.html' /*, $dataControlModelHome*/ );
+    return $this->view->render($response, 'home.html');
 
 });
 
