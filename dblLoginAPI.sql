74fe3d1d568101fe5c8307bb67d92099ed6c062e
Hedrei Andrade <hedreiandrade@gmail.com>
2025-09-25 09:02:05 -0300

    Criando tabela de likes

--- dblLoginAPI.sql
+++ dblLoginAPI.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `followers` (
   CONSTRAINT `fk_followers_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela api-doeobem.followers: ~42 rows (aproximadamente)
+-- Copiando dados para a tabela api-doeobem.followers: ~47 rows (aproximadamente)
 /*!40000 ALTER TABLE `followers` DISABLE KEYS */;
 INSERT INTO `followers` (`id`, `user_id`, `follower_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
 	(1, 85, 84, '2025-03-30 03:12:43', NULL, NULL),
@@ -97,6 +97,8 @@ CREATE TABLE IF NOT EXISTS `likes` (
 
 -- Copiando dados para a tabela api-doeobem.likes: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `likes` DISABLE KEYS */;
+INSERT INTO `likes` (`id`, `post_id`, `user_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(4, 1, 106, '2025-09-25 11:53:44', '2025-09-25 11:53:57', '2025-09-25 11:53:57');
 /*!40000 ALTER TABLE `likes` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela api-doeobem.posts
@@ -110,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Copiando dados para a tabela api-doeobem.posts: ~8 rows (aproximadamente)
+-- Copiando dados para a tabela api-doeobem.posts: ~10 rows (aproximadamente)
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
 INSERT INTO `posts` (`id`, `description`, `media_link`, `created_at`, `updated_at`, `deleted_at`) VALUES
 	(1, 'Desse FDS', 'http://localhost:8009/public/images/profile/286959604euuu.jpg', '2025-03-30 03:10:07', NULL, NULL),
@@ -140,7 +142,7 @@ CREATE TABLE IF NOT EXISTS `posts_users` (
   CONSTRAINT `FK__users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela api-doeobem.posts_users: ~8 rows (aproximadamente)
+-- Copiando dados para a tabela api-doeobem.posts_users: ~10 rows (aproximadamente)
 /*!40000 ALTER TABLE `posts_users` DISABLE KEYS */;
 INSERT INTO `posts_users` (`id`, `user_id`, `post_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
 	(1, 85, 1, '2025-03-30 03:10:36', NULL, NULL),
@@ -187,7 +189,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=115 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Copiando dados para a tabela api-doeobem.users: ~28 rows (aproximadamente)
+-- Copiando dados para a tabela api-doeobem.users: ~29 rows (aproximadamente)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `name`, `nickname`, `phone1`, `has_whatsapp`, `phone2`, `email`, `password`, `postal_code`, `street`, `number`, `complement`, `neighborhood`, `city`, `state`, `country`, `recovery_key`, `first_access`, `last_access`, `access_count`, `receive_newsletter`, `active`, `created_at`, `updated_at`, `deleted_at`, `userscol`, `photo`) VALUES
 	(60, 'Admin', NULL, NULL, 0, NULL, 'admin@user.com', '$2y$10$DBRiDXdHGicq9HJYnKxODOXsQvWDLc0rU4rUuqfOr/SRNukwS06NC', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 1, '2025-03-08 19:02:15', '2025-03-08 19:02:15', NULL, NULL, 'http://localhost:8009/public/images/profile/978727390euuu.jpg'),
